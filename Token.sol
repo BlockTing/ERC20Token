2bae43004b0cb67caa6b2db5de033847fefcf599
BlockTing <netan@blockting.com>
2021-12-28 15:35:28 +0530

    modified token contract

--- Token.sol
+++ Token.sol
@@ -9,15 +9,32 @@ import "./utils/AccessControl.sol";
  * @notice mintable only by admins, no approval needed for a contract
  */
 contract ERC20Key is ERC20, AccessControl {
+    address internal stakingContractAddress;
+    address internal swappingContractAddress;
+    address internal nftContractAddress;
+    address internal influeqContractAddress;
 
     /**
      * @dev Constructor
+     * @param _stakingContractAddress upgradable address
+     * @param _swappingContractAddress upgradable address
      */
     constructor(
         string memory name,
-        string memory symbol
+        string memory symbol,
+        uint _amount,
+        address _stakingContractAddress,
+        address _swappingContractAddress,
+        address _nftContractAddress,
+        address _influeqContractAddress
     ) ERC20(name, symbol) {
+        stakingContractAddress = _stakingContractAddress;
+        swappingContractAddress = _swappingContractAddress;
+        nftContractAddress = _nftContractAddress;
+        influeqContractAddress = _influeqContractAddress;
+
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _mint(_msgSender(), _amount);
     }
 
     function mint(address account, uint256 amount)
@@ -34,5 +51,80 @@ contract ERC20Key is ERC20, AccessControl {
         for (uint256 j = 0; j < accounts.length; j++) {
             _mint(accounts[j], amounts[j]);
         }
+    }
+
+    function getSwappingContractAddress()
+    public
+    view
+    returns (address)
+    {
+        return swappingContractAddress;
+    }
+
+    function getStakingContractAddress()
+    public
+    view
+    returns (address)
+    {
+        return stakingContractAddress;
+    }
+    
+    function getNftContractAddress()
+    public
+    view
+    returns (address)
+    {
+        return nftContractAddress;
+    }
+    
+    function getInflueqContractAddress()
+    public
+    view
+    returns (address)
+    {
+        return influeqContractAddress;
+    }
+
+    function setSwappingContractAddress(address _swappingContractAddress)
+    external
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(_swappingContractAddress != swappingContractAddress);
+        swappingContractAddress = _swappingContractAddress;
+    }
+
+    function setStakingContractAddress(address _stakingContractAddress)
+    external
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(_stakingContractAddress != stakingContractAddress);
+        stakingContractAddress = _stakingContractAddress;
+    }
+
+    function setNftContractAddress(address _nftContractAddress)
+    external
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(_nftContractAddress != nftContractAddress);
+        nftContractAddress = _nftContractAddress;
+    }
+
+    function setInflueqContractAddress(address _influeqContractAddress)
+    external
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(_influeqContractAddress != influeqContractAddress);
+        influeqContractAddress = _influeqContractAddress;
+    }
+
+    function selfDestructContract(address _sendAllEth)
+    external
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        selfdestruct(payable(_sendAllEth));
+    }
+
+    receive() external payable {
+        revert();
     }
 }
